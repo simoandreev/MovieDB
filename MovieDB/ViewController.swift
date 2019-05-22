5466bd39fe5c5bb3b5680fbad7639db7efd1b605
Simeon Andreev <simo1914@abv.bg>
2019-05-22 22:40:33 +0300

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Simeon Andreev on 22.05.19.
+//  Copyright © 2019 Simeon Andreev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
